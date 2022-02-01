b11477202deee438f22515b2cb22d33f29c14162
Marvin Winkler <m@rvin.at>
2022-02-01 18:36:05 +0100

    small fixes

--- scripts/mysql-backup-helper.sh
+++ scripts/mysql-backup-helper.sh
@@ -9,12 +9,15 @@ then
 fi
 
 RESTORE_PATH="$BACKUP_PATH/to_restore"
+RESTORED_PATH="$BACKUP_PATH/restored"
 
 echo ">> using \$BACKUP_PATH: $BACKUP_PATH"
 echo ">> using \$RESTORE_PATH: $RESTORE_PATH"
+echo ">> using \$RESTORED_PATH: $RESTORED_PATH"
 
 mkdir -p "$BACKUP_PATH" &> /dev/null
 mkdir -p "$RESTORE_PATH" &> /dev/null
+mkdir -p "$RESTORED_PATH" &> /dev/null
 
 DB_LIST=`echo "show databases;" | mysql --defaults-extra-file="$MYSQL_DEFAULTS_FILE" | tail -n +2 | grep -v "information_schema\|performance_schema"`
 
@@ -44,8 +47,7 @@ for file in $(find "$RESTORE_PATH"/*.sql -type f); do
     echo -n "    >> dropping old $FILE_DB_NAME... "
     echo "DROP DATABASE $FILE_DB_NAME;" | mysql --defaults-extra-file="$MYSQL_DEFAULTS_FILE"
     echo -n "    >> importing new $FILE_DB_NAME... "
-    mysql --defaults-extra-file="$MYSQL_DEFAULTS_FILE" "$FILE_DB_NAME" < "$file"
-    rm -f "$file"
+    mysql --defaults-extra-file="$MYSQL_DEFAULTS_FILE" "$FILE_DB_NAME" < "$file" && mv "$file" "$RESTORED_PATH/"
     echo "done"
   fi
 done
